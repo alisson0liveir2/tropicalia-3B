21cbbdb32f45b50aace1acdb3ea9a69941a77444
alisson0liveir2 <145785362+alisson0liveir2@users.noreply.github.com>
2025-04-01 08:30:59 -0300

    32/03

--- script.js
+++ script.js
@@ -2,5 +2,9 @@
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
 
     let tamanhaAtualFonte = 1;
-    
-})
+
+    aumentaFonteBotao.addEventListener('click', function(){
+        tamanhaAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhaAtualFonte}rem`;
+    });
+});
