7b9e1af6c99e80744d810dc705ad3316ada6054c
alisson0liveir2 <145785362+alisson0liveir2@users.noreply.github.com>
2025-04-15 08:27:36 -0300

    w

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
  document.addEventListener('DOMContentLoaded', function(){
+     
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+
+    botaoDeAcessibilidade.addEventListener('click', function(){
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.toggle('apresenta-lista')
+    })
+
+
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
@@ -12,6 +22,5 @@
     diminuiFonteBotao.addEventListener('click', function(){
         tamanhaAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhaAtualFonte}rem`;
-
-
+    });
 });
