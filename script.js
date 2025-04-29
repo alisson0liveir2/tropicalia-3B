b26a67ab3635920b8f1cca0e6db72856d769f4b1
alisson0liveir2 <145785362+alisson0liveir2@users.noreply.github.com>
2025-04-29 08:24:25 -0300

    e

--- script.js
+++ script.js
@@ -33,3 +33,10 @@
     })
 
 });
+
+ScrollReveal().reveal('#inicio',{delay: 500});
+ScrollReveal().reveal('#galeria',{delay: 500});
+ScrollReveal().reveal('#tropicalia',{delay: 500});
+ScrollReveal().reveal('#contato',{delay: 500});
+
+
